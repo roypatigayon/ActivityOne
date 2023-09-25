f57b4666b017e074660cfcecb318e66002aa6605
Roy Nino Patigayon <roynino.patigayon@cit.edu>
2023-09-25 19:07:38 +0800

    add sample git code

--- src/activityone/ActivityOne.java
+++ src/activityone/ActivityOne.java
@@ -14,7 +14,8 @@ public class ActivityOne {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
+        System.out.println("Sample Git commit and push");
     }
     
 }
